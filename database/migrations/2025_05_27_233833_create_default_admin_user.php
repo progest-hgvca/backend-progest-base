5684d3ca1ef6782a0a43b43804edaf719f6e9851
David Júnior <davidjunior051204@gmail.com>
2025-07-07 11:41:33 -0300

    Rodar no ambiente de David

--- database/migrations/2025_05_27_233833_create_default_admin_user.php
+++ database/migrations/2025_05_27_233833_create_default_admin_user.php
@@ -24,6 +24,7 @@ class CreateDefaultAdminUser extends Migration
             'data_nascimento' => '1990-01-01',
             'cpf' => '00000000000',
             'unidade_consumidora_id' => 1,
+            'usuario_tipo' => 1,
             'status' => 'A',
             'created_at' => now(),
             'updated_at' => now(),
