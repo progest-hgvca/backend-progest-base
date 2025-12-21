9f9cd919a2ae59288a98cd9d8b6d9f40d8ba6707
David Júnior <davidjunior051204@gmail.com>
2025-12-21 18:43:37 -0300

    feat: Add CORS configuration and update example API URL protocol from HTTPS to HTTP.

--- config/cors.php
+++ config/cors.php
@@ -17,15 +17,11 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie', '/user/add'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'http://localhost:5173',
-        'https://hgvcprogrest.davidjunior.dev.br',
-        'http://hgvcprogrest.davidjunior.dev.br',
-    ],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 
 ];
